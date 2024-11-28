8004d32461cddfa21243918418c2576144e49676
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2024-11-28 01:35:25 -0600

    more debug

--- cmake/public/cuda.cmake
+++ cmake/public/cuda.cmake
@@ -133,7 +133,8 @@ if(CUDA_NVRTC_LIB AND NOT CUDA_NVRTC_SHORTHASH)
   find_package(Python COMPONENTS Interpreter)
   execute_process(
     COMMAND Python::Interpreter -c
-    "import hashlib;hash=hashlib.sha256();print(f'${CUDA_NVRTC_LIB}');hash.update(open('${CUDA_NVRTC_LIB}','rb').read());print(hash.hexdigest()[:8])"
+    "import hashlib;hash=hashlib.sha256();print(f'CUDA_NVRTC_LIB=${CUDA_NVRTC_LIB}')"
+    # "import hashlib;hash=hashlib.sha256();print(f'${CUDA_NVRTC_LIB}');hash.update(open('${CUDA_NVRTC_LIB}','rb').read());print(hash.hexdigest()[:8])"
     RESULT_VARIABLE _retval
     OUTPUT_VARIABLE CUDA_NVRTC_SHORTHASH)
   message(STATUS "Python_EXECUTABLE=(${Python_EXECUTABLE}); Python_INTERPRETER=(${Python_INTERPRETER}); _retval=${_retval}. CUDA_NVRTC_LIB=${CUDA_NVRTC_LIB}")
